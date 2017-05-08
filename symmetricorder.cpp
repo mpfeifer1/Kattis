b96b95abab1d04e8e003544f5a65965d1608c87e
Michael Pfeifer <mikepf97@gmail.com>
2017-05-08 00:25:11 -0600

    Solved one more

--- symmetricorder.cpp
+++ symmetricorder.cpp
@@ -5,21 +5,40 @@
 using namespace std;
 
 int main() {
-    while(true) {
-        int n;
-        cin >> n;
-        if(n == 0) {
-            return 0;
-        }
-
+    int n;
+    int j = 0;
+    while(cin >> n && n != 0) {
+        j++;
         vector<string> v;
-
         for(int i = 0; i < n; i++) {
-            string temp;
-            cin >> temp;
-            v.push_back(temp);
+            string s;
+            cin >> s;
+            v.push_back(s);
         }
 
-        vector<string> o;
+        cout << "SET " << j << endl;
+
+        if(v.size() % 2 == 1) {
+            // odd
+            int i = 0;
+            for(i; i < v.size(); i+=2) {
+                cout << v[i] << endl;
+            }
+            i -= 3;
+            for(i; i >= 0; i-=2) {
+                cout << v[i] << endl;
+            }
+        }
+        else {
+            // even
+            int i = 0;
+            for(i; i < v.size(); i+=2) {
+                cout << v[i] << endl;
+            }
+            i -= 1;
+            for(i; i >= 0; i-=2) {
+                cout << v[i] << endl;
+            }
+        }
     }
 }
