51861a4c161da89a25744980501c253ec30dc03c
Michael Pfeifer <mikepf97@gmail.com>
2017-05-13 12:17:37 -0600

    Finished Zoo. Unordered Map and Set don't keep track of count. Use multimap

---
+++ zoo.cpp
@@ -0,0 +1,49 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+string lastWord(string s) {
+    int index = s.find_last_of(' ') + 1;
+
+    string last = s.substr(index);
+
+    for(auto &c : last) {
+        c = tolower(c);
+    }
+
+    return last;
+}
+
+int main() {
+    int n;
+    int i = 1;
+    while(cin >> n && n != 0) {
+        unordered_multimap<string, int> animals;
+        vector<string> names;
+
+        cin.ignore();
+        for(int i = 0; i < n; i++) {
+            string name;
+            getline(cin, name);
+            name = lastWord(name);
+
+            if(animals.count(name) == 0) {
+                names.push_back(name);
+            }
+            animals.insert({name, 0});
+        }
+
+        sort(names.begin(), names.end());
+
+        cout << "List " << i << ":" << endl;
+        for(auto name : names) {
+            cout << name << " | " << animals.count(name) << endl;
+        }
+
+        i++;
+    }
+}
