fd146d48a2d6dc4103a8c99539d5a544a2bf46bb
Michael Pfeifer <mikepf97@gmail.com>
2017-10-19 18:57:41 -0600

    Solved Flying Safely

---
+++ flyingsafely.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int cases;
+    cin >> cases;
+
+    for(int i = 0; i < cases; i++) {
+        int cities, pilots;
+        cin >> cities >> pilots;
+
+        for(int j = 0; j < pilots; j++) {
+            int a, b;
+            cin >> a >> b;
+        }
+
+        cout << cities - 1 << endl;
+    }
+}
