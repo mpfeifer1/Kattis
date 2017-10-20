9ce34b98a93a286bcdd377b5bb8d744d3521fe65
Michael Pfeifer <mikepf97@gmail.com>
2017-10-19 23:06:12 -0600

    Solved Baloni

---
+++ baloni.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> counts;
+    counts.resize(1000001, 0);
+
+    for(int i = 0; i < n; i++) {
+        int temp;
+        cin >> temp;
+
+        if(counts[temp] == 0) {
+            counts[temp-1]++;
+        }
+        else {
+            counts[temp-1]++;
+            counts[temp]--;
+        }
+    }
+
+    int throws = 0;
+    for(int i = 0; i < 1000000; i++) {
+        throws += counts[i];
+    }
+    cout << throws << endl;
+}
