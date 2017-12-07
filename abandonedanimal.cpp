89044a00534c532d86766c5e8f30d23289bb1857
Michael Pfeifer <mikepf97@gmail.com>
2017-12-06 23:29:18 -0700

    Solved Abandoned Animal

--- abandonedanimal.cpp
+++ abandonedanimal.cpp
@@ -1,14 +1,14 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
+int inf = 1 << 30;
 
 int main() {
-    int n, k, m;
+    int n, k;
     cin >> n >> k;
 
     // Set up data structures
-    unordered_map<string, int> item_to_num;
-    vector<vector<int>> adj(n);
+    unordered_map<string, set<int>> m;
     vector<int> items;
 
     // Read in the first half of the data
@@ -16,20 +16,77 @@ int main() {
         int store;
         string item;
         cin >> store >> item;
-        if(item_to_num.count(item) == 0) {
-            int size = item_to_num.size();
-            item_to_num[item] = size;
-        }
-
-        adj[store].push_back(item_to_num[s])
+        m[item].insert(store);
     }
 
+    // Keep track of input
+    vector<string> input;
+
     // Read in the second half
-    int m;
-    cin >> m;
-    for(int i = 0; i < m; i++) {
+    int q;
+    cin >> q;
+    for(int i = 0; i < q; i++) {
         string item;
         cin >> item;
-        items.push_back(item);
+        input.push_back(item);
+    }
+
+    // Get max from lo to hi
+    int maxhere = 0;
+    for(int i = 0; i < q; i++) {
+        string curr = input[i];
+        vector<int> rem;
+        for(auto i : m[curr]) {
+            if(i < maxhere) {
+                rem.push_back(i);
+            }
+        }
+        for(auto i : rem) {
+            m[curr].erase(i);
+        }
+        if(m[curr].size() > 0) {
+            maxhere = max(*m[curr].begin(), maxhere);
+        }
+    }
+
+    // Get min from hi to lo
+    int minhere = inf;
+    for(int i = q-1; i >= 0; i--) {
+        string curr = input[i];
+        vector<int> rem;
+        for(auto i : m[curr]) {
+            if(i > minhere) {
+                rem.push_back(i);
+            }
+        }
+        for(auto i : rem) {
+            m[curr].erase(i);
+        }
+        if(m[curr].size() > 0) {
+            minhere = min(*m[curr].rbegin(), minhere);
+        }
+    }
+
+    // Find answer
+    bool works = true;
+    bool ambig = false;
+    for(auto i : input) {
+        if(m[i].size() < 1) {
+            works = false;
+        }
+        if(m[i].size() > 1) {
+            ambig = true;
+        }
+    }
+
+    // Print answer
+    if(!works) {
+        cout << "impossible" << endl;
+    }
+    else if(ambig) {
+        cout << "ambiguous" << endl;
+    }
+    else {
+        cout << "unique" << endl;
     }
 }
