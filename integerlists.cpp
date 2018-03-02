6a4d9f0402c5df711dab783d43d08884c7f94a75
Michael Pfeifer <mikepf97@gmail.com>
2018-03-01 21:37:56 -0700

    Solved Integer Lists

---
+++ integerlists.cpp
@@ -0,0 +1,124 @@
+// Michael Pfeifer
+// Version 04, 2018/02/17
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned int uint;
+typedef unsigned long long ull;
+
+const ld  pi   = 4.0*atanl(1.0);
+const int iinf = 1e9 + 10;
+const ll  inf  = 1e18 + 10;
+const int mod  = 1000000007;
+const ld  prec = .000001;
+
+#define enld endl
+#define endl '\n'
+#define pb push_back
+#define debug(x) cout<<#x<<" -> "<<x<<'\n'
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define uni(x) (x).erase(unique(all(x)), (x).end())
+#define rep(i, n) for (ll i = 0; i < (ll)(n); ++i)
+#define rep1(i, n) for (ll i = 1; i <= (ll)(n); ++i)
+#define umap unordered_map
+#define uset unordered_set
+
+template<class TIn>
+using indexed_set = tree<
+        TIn, null_type, less<TIn>,
+        rb_tree_tag, tree_order_statistics_node_update>;
+
+void fast() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+}
+
+void file() {
+    auto a = freopen("a.in",  "r", stdin);
+    auto b = freopen("a.out", "w", stdout);
+    if(!a || !b) cout << "uh oh" << endl;
+}
+
+
+
+
+
+int main() {
+    //file();
+    //fast();
+
+    int cases;
+    cin >> cases;
+    while(cases--) {
+        string command;
+        cin >> command;
+
+        int n;
+        cin >> n;
+
+        deque<int> d;
+        cin.ignore();
+        for(int i = 0; i < n; i++) {
+            cin.ignore();
+            int t;
+            cin >> t;
+            d.pb(t);
+        }
+        cin.ignore();
+        if(n == 0) {
+            cin.ignore();
+        }
+
+        bool beg = true;
+        bool printed = false;
+        for(auto i : command) {
+            if(i == 'R') {
+                beg = !beg;
+            }
+            if(i == 'D') {
+                if(d.size() == 0) {
+                    printed = true;
+                    cout << "error" << endl;
+                    break;
+                }
+                else {
+                    if(beg) {
+                        d.pop_front();
+                    }
+                    else {
+                        d.pop_back();
+                    }
+                }
+            }
+        }
+
+        if(!printed) {
+            cout << "[";
+            while(!d.empty()) {
+                if(beg) {
+                    cout << d.front();
+                    d.pop_front();
+                }
+                else {
+                    cout << d.back();
+                    d.pop_back();
+                }
+
+                if(d.size() != 0) {
+                    cout << ",";
+                }
+            }
+            cout << "]" << endl;
+        }
+    }
+
+    return 0;
+}
