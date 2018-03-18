804bc29114eb328261df51dc3e8cdd35ee4bf246
Michael Pfeifer <mikepf97@gmail.com>
2018-03-18 09:16:45 -0600

    Solved To Lower

---
+++ tolower.cpp
@@ -0,0 +1,82 @@
+// Michael Pfeifer
+// Version 04, 2018/02/17
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned int uint;
+typedef unsigned long long ull;
+
+const ld  pi   = 4.0*atanl(1.0);
+const int iinf = 1e9 + 10;
+const ll  inf  = 1e18 + 10;
+const int mod  = 1000000007;
+const ld  prec = .000001;
+
+#define enld endl
+#define endl '\n'
+#define pb push_back
+#define debug(x) cout<<#x<<" -> "<<x<<'\n'
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define uni(x) (x).erase(unique(all(x)), (x).end())
+#define rep(i, n) for (ll i = 0; i < (ll)(n); ++i)
+#define rep1(i, n) for (ll i = 1; i <= (ll)(n); ++i)
+#define umap unordered_map
+#define uset unordered_set
+
+template<class TIn>
+using indexed_set = tree<
+        TIn, null_type, less<TIn>,
+        rb_tree_tag, tree_order_statistics_node_update>;
+
+void fast() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+}
+
+void file() {
+    auto a = freopen("a.in",  "r", stdin);
+    auto b = freopen("a.out", "w", stdout);
+    if(!a || !b) cout << "uh oh" << endl;
+}
+
+
+
+
+
+int main() {
+    //file();
+    //fast();
+
+    int n, m;
+    cin >> n >> m;
+
+    int ans = 0;
+    for(int i = 0; i < n; i++) {
+        bool works = true;
+        for(int j = 0; j < m; j++) {
+            string s;
+            cin >> s;
+
+            for(int k = 1; k < s.size(); k++) {
+                if(s[k] != tolower(s[k])) {
+                    works = false;
+                }
+            }
+        }
+        if(works) {
+            ans++;
+        }
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
