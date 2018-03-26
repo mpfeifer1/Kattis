426c0034c40c1bf7e71011e48b9ea1646885935a
Michael Pfeifer <mikepf97@gmail.com>
2018-03-26 10:13:44 -0600

    Solved Pseudoprime

---
+++ pseudoprime.cpp
@@ -0,0 +1,101 @@
+// Michael Pfeifer
+// Version 04, 2018/02/17
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned int uint;
+typedef unsigned long long ull;
+
+const ld  pi   = 4.0*atanl(1.0);
+const int iinf = 1e9 + 10;
+const ll  inf  = 1e18 + 10;
+const int mod  = 1000000007;
+const ld  prec = .000001;
+
+#define enld endl
+#define endl '\n'
+#define pb push_back
+#define debug(x) cout<<#x<<" -> "<<x<<'\n'
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define uni(x) (x).erase(unique(all(x)), (x).end())
+#define rep(i, n) for (ll i = 0; i < (ll)(n); ++i)
+#define rep1(i, n) for (ll i = 1; i <= (ll)(n); ++i)
+#define umap unordered_map
+#define uset unordered_set
+
+template<class TIn>
+using indexed_set = tree<
+        TIn, null_type, less<TIn>,
+        rb_tree_tag, tree_order_statistics_node_update>;
+
+void fast() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+}
+
+void file() {
+    auto a = freopen("a.in",  "r", stdin);
+    auto b = freopen("a.out", "w", stdout);
+    if(!a || !b) cout << "uh oh" << endl;
+}
+
+
+
+
+
+ll fastpow(ll base, ll exp, ll mod) {
+    ll res = 1;
+    while(exp > 0) {
+        if((exp & 1) == 1) {
+            res = (res * base) % mod;
+        }
+        base = (base * base) % mod;
+        exp /= 2;
+    }
+
+    return res % mod;
+}
+
+bool isprime(ll n) {
+    if(n == 2) {
+        return true;
+    }
+    if(n < 2) {
+        return false;
+    }
+    for(ll i = 2; i <= sqrt(n); i++) {
+        if(n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    //file();
+    //fast();
+
+    ll prime, base;
+    while(cin >> prime && cin >> base) {
+        if(prime == 0 && base == 0) {
+            break;
+        }
+
+        if(fastpow(base, prime, prime) == base && !isprime(prime)) {
+            cout << "yes" << endl;
+        }
+        else {
+            cout << "no" << endl;
+        }
+    }
+
+    return 0;
+}
