cde478475fcbcb85e0ae2daa4acb1a7c5c7e9f58
Michael Pfeifer <mikepf97@gmail.com>
2018-04-14 21:58:19 -0600

    Solved Workstations

---
+++ workstations.cpp
@@ -0,0 +1,100 @@
+// Michael Pfeifer
+// Version 04, 2018/02/17
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned int uint;
+typedef unsigned long long ull;
+
+const ld  pi   = 4.0*atanl(1.0);
+const int iinf = 1e9 + 10;
+const ll  inf  = 1e18 + 10;
+const int mod  = 1000000007;
+const ld  prec = .000001;
+
+#define enld endl
+#define endl '\n'
+#define pb push_back
+#define debug(x) cout<<#x<<" -> "<<x<<'\n'
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define uni(x) (x).erase(unique(all(x)), (x).end())
+#define rep(i, n) for (ll i = 0; i < (ll)(n); ++i)
+#define rep1(i, n) for (ll i = 1; i <= (ll)(n); ++i)
+#define umap unordered_map
+#define uset unordered_set
+
+template<class TIn>
+using indexed_set = tree<
+        TIn, null_type, less<TIn>,
+        rb_tree_tag, tree_order_statistics_node_update>;
+
+void fast() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+}
+
+void file() {
+    auto a = freopen("a.in",  "r", stdin);
+    auto b = freopen("a.out", "w", stdout);
+    if(!a || !b) cout << "uh oh" << endl;
+}
+
+
+
+
+
+struct event {
+    ll start, end, len;
+};
+
+bool cmp(event& e1, event& e2) {
+    if(e1.start == e2.start) {
+        return e1.len < e2.len;
+    }
+    return e1.start < e2.start;
+}
+
+int main() {
+    //file();
+    fast();
+
+    ll n, m;
+    cin >> n >> m;
+
+    vector<event> v(n);
+    for(auto& i : v) {
+        cin >> i.start >> i.len;
+    }
+    sort(all(v), cmp);
+
+    multiset<ll> endtimes;
+    ll saves = 0;
+    for(auto i : v) {
+        while(!endtimes.empty()) {
+            if(*endtimes.begin() > i.start) {
+                break;
+            }
+            if(*endtimes.begin() + m >= i.start) {
+                endtimes.erase(endtimes.begin());
+                saves++;
+                break;
+            }
+            endtimes.erase(endtimes.begin());
+        }
+        endtimes.insert(i.start + i.len);
+    }
+
+    cout << saves << endl;
+
+    return 0;
+}
+
+
