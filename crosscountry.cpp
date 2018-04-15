a8f63a2a369f4140b401a05d80e4eb98765445ee
Michael Pfeifer <mikepf97@gmail.com>
2018-04-15 12:30:45 -0600

    Solved Cross Country

---
+++ crosscountry.cpp
@@ -0,0 +1,91 @@
+// Michael Pfeifer
+// Version 04, 2018/02/17
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned int uint;
+typedef unsigned long long ull;
+
+const ld  pi   = 4.0*atanl(1.0);
+const int iinf = 1e9 + 10;
+const ll  inf  = 1e18 + 10;
+const int mod  = 1000000007;
+const ld  prec = .000001;
+
+#define enld endl
+#define endl '\n'
+#define pb push_back
+#define debug(x) cout<<#x<<" -> "<<x<<'\n'
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define uni(x) (x).erase(unique(all(x)), (x).end())
+#define rep(i, n) for (ll i = 0; i < (ll)(n); ++i)
+#define rep1(i, n) for (ll i = 1; i <= (ll)(n); ++i)
+#define umap unordered_map
+#define uset unordered_set
+
+template<class TIn>
+using indexed_set = tree<
+        TIn, null_type, less<TIn>,
+        rb_tree_tag, tree_order_statistics_node_update>;
+
+void fast() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+}
+
+void file() {
+    auto a = freopen("a.in",  "r", stdin);
+    auto b = freopen("a.out", "w", stdout);
+    if(!a || !b) cout << "uh oh" << endl;
+}
+
+
+
+
+
+int main() {
+    //file();
+    //fast();
+
+    int n, s, t;
+    cin >> n >> s >> t;
+
+    vector<vector<int>> adj;
+    adj.resize(n, vector<int>(n));
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cin >> adj[i][j];
+        }
+    }
+
+    vector<int> dist(n, iinf);
+    set<pair<int,int>> q;
+    q.insert({0,s});
+    dist[s] = 0;
+
+    while(!q.empty()) {
+        int curr = (*q.begin()).second;
+        q.erase(q.begin());
+
+        for(int next = 0; next < n; next++) {
+            if(next == curr) continue;
+            if(dist[next] > dist[curr] + adj[curr][next]) {
+                q.erase({dist[next], next});
+                dist[next] = dist[curr] + adj[curr][next];
+                q.insert({dist[next], next});
+            }
+        }
+    }
+
+    cout << dist[t] << endl;
+
+    return 0;
+}
