c7d1b425265e97439d2ed21cb6ef710f503ba2ba
Michael Pfeifer <mikepf97@gmail.com>
2018-04-25 21:52:10 -0600

    Solved Book Circle

---
+++ bookcircle.cpp
@@ -0,0 +1,136 @@
+// Michael Pfeifer
+// Version 04, 2018/02/17
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned int uint;
+typedef unsigned long long ull;
+
+const ld  pi   = 4.0*atanl(1.0);
+const int iinf = 1e9 + 10;
+const ll  inf  = 1e18 + 10;
+const int mod  = 1000000007;
+const ld  prec = .000001;
+
+#define enld endl
+#define endl '\n'
+#define pb push_back
+#define debug(x) cout<<#x<<" -> "<<x<<'\n'
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define uni(x) (x).erase(unique(all(x)), (x).end())
+#define rep(i, n) for (ll i = 0; i < (ll)(n); ++i)
+#define rep1(i, n) for (ll i = 1; i <= (ll)(n); ++i)
+#define umap unordered_map
+#define uset unordered_set
+
+template<class TIn>
+using indexed_set = tree<
+        TIn, null_type, less<TIn>,
+        rb_tree_tag, tree_order_statistics_node_update>;
+
+void fast() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+}
+
+void file() {
+    auto a = freopen("a.in",  "r", stdin);
+    auto b = freopen("a.out", "w", stdout);
+    if(!a || !b) cout << "uh oh" << endl;
+}
+
+
+
+
+
+bool match(int curr, vector<vector<int>>& adj, vector<int>& l, vector<int>& r, vector<int>& vis) {
+    if(vis[curr]) return false;
+    vis[curr] = true;
+
+    for(auto next : adj[curr]) {
+        if(r[next] == -1 || match(r[next], adj, l, r, vis)) {
+            l[curr] = next;
+            r[next] = curr;
+            return true;
+        }
+    }
+    return false;
+}
+
+int bipartite(vector<vector<int>>& adj, int n, int m) {
+    vector<int> l, r, vis;
+    l.resize(n, -1);
+    r.resize(m, -1);
+    vis.resize(n);
+
+    bool works = true;
+    while(works) {
+        works = false;
+        fill(vis.begin(), vis.end(), 0);
+        for(int i = 0; i < n; i++) {
+            if(l[i] == -1) {
+                works |= match(i, adj, l, r, vis);
+            }
+        }
+    }
+
+    int ret = 0;
+    for(int i = 0; i < n; i++) {
+        ret += (l[i] != -1);
+    }
+    return ret;
+}
+
+int main() {
+    //file();
+    fast();
+
+    int b, g;
+    cin >> b >> g;
+
+    vector<pair<int,int>> book;
+    map<string,int> conv;
+
+    for(int i = 0; i < b+g; i++) {
+        string t;
+        cin >> t;
+
+        int q;
+        cin >> q;
+        for(int j = 0; j < q; j++) {
+            string curr;
+            cin >> curr;
+            if(conv.count(curr) == 0) {
+                int sz = conv.size();
+                conv[curr] = sz;
+                book.pb({0,0});
+            }
+            int val = conv[curr];
+
+            if(i < b) {
+                book[val].first = i;
+            }
+            else {
+                book[val].second = i-b;
+            }
+        }
+    }
+
+    // Set up the adjacency matrix
+    vector<vector<int>> adj(b);
+    for(auto i : book) {
+        adj[i.first].pb(i.second);
+    }
+
+    cout << bipartite(adj, b, g) << endl;
+
+    return 0;
+}
