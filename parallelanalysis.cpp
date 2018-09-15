c33c00e7d46e193f29d45f82689a35b1cc889ad9
Michael Pfeifer <mikepf97@gmail.com>
2018-09-15 16:33:41 -0600

    Solved Parallel Analysis

---
+++ parallelanalysis.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+
+    int best = 0;
+    map<int,int> m;
+    for(int i = 0; i < n; i++) {
+        int t;
+        cin >> t;
+        int besthere = 0;
+        int v;
+        for(int j = 0; j < t-1; j++) {
+            cin >> v;
+            besthere = max(besthere, m[v]);
+        }
+        cin >> v;
+        besthere++;
+        m[v] = besthere;
+        best = max(best, besthere);
+    }
+
+    cout << best << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int cases;
+    cin >> cases;
+    for(int i = 1; i <= cases; i++) {
+        cout << i << " ";
+        solve();
+    }
+}
