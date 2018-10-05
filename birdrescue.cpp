9e0968309e252ae3aca5518333e858cf0d77be0b
Michael Pfeifer <mikepf97@gmail.com>
2018-10-04 22:32:11 -0600

    Switched back to ints from longs

--- birdrescue.cpp
+++ birdrescue.cpp
@@ -1,9 +1,8 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-typedef long long ll;
 
-ll manhattan(ll x1, ll y1, ll x2, ll y2) {
+int manhattan(int x1, int y1, int x2, int y2) {
     return abs(x1-x2) + abs(y1-y2);
 }
 
@@ -11,17 +10,17 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
 
-    ll n, q;
+    int n, q;
     cin >> n >> q;
 
-    ll px, py;
+    int px, py;
     cin >> px >> py;
 
-    vector<ll> dist(2000002, 0);
+    vector<int> dist(2000002, 0);
 
-    for(ll i = 0; i < n; i++) {
-        vector<ll> x(2);
-        vector<ll> y(2);
+    for(int i = 0; i < n; i++) {
+        vector<int> x(2);
+        vector<int> y(2);
         cin >> x[0] >> y[0] >> x[1] >> y[1];
         sort(x.begin(), x.end());
         sort(y.begin(), y.end());
@@ -33,7 +32,7 @@ int main() {
             y.push_back(py);
         }
 
-        vector<ll> dists;
+        vector<int> dists;
         for(auto j : x) {
             for(auto k : y) {
                 dists.push_back(manhattan(j, k, px, py));
@@ -49,14 +48,14 @@ int main() {
         }
     }
 
-    ll sum = 0;
-    for(ll i = 0; i < dist.size(); i++) {
+    int sum = 0;
+    for(int i = 0; i < dist.size(); i++) {
         sum += dist[i];
         dist[i] = sum;
     }
 
-    for(ll i = 0; i < q; i++) {
-        ll t;
+    for(int i = 0; i < q; i++) {
+        int t;
         cin >> t;
         cout << dist[t] << endl;
     }
