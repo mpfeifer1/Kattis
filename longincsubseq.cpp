8d3cbed154e92034dadb8665a12cf65729a44ad1
Michael Pfeifer <mikepf97@gmail.com>
2018-10-29 21:40:47 -0600

    Solved Longest Increasing Subsequence

--- longincsubseq.cpp
+++ longincsubseq.cpp
@@ -1,81 +1,60 @@
-#include <algorithm>
-#include <iostream>
-#include <vector>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int search(vector<int> v, vector<int>& t, int l, int r, int key) {
-    // Binary search
-    while(r-l > 1) {
-        int m = l + (r-l)/2;
-        if(v[t[m]] >= key) {
-            r = m;
+// Returns an array with the indexes of the LIS
+template <class T>
+vector<int> LIS(vector<T>& v) {
+    vector<int> p(v.size(), -1);
+    vector<int> t(v.size(), 0);
+
+    int lis = 1;
+    for(int i = 1; i < v.size(); i++) {
+        if(v[i] <= v[t[0]]) {
+            t[0] = i;
+        }
+        else if(v[i] > v[t[lis - 1]]) {
+            p[i] = t[lis - 1];
+            t[lis++] = i;
         }
         else {
-            l = m;
+            int l = -1;
+            int r = lis - 1;
+            while(r - l > 1) {
+                int m = l + (r - l) / 2;
+                if(v[t[m]] >= v[i]) r = m;
+                else l = m;
+            }
+            p[i] = t[r - 1];
+            t[r] = i;
         }
     }
 
-    return r;
+    vector<int> ans;
+    for(int i = t[lis - 1]; i >= 0; i = p[i]) {
+        ans.push_back(i);
+    }
+    reverse(ans.begin(), ans.end());
+
+    return ans;
 }
 
 int main() {
     int n;
+
     while(cin >> n) {
-        // If array of size 0, print 0
-        if(n == 0) {
-            cout << 0 << endl << endl;
-            continue;
-        }
+        // Read in array
+        vector<int> arr(n,0);
+        for(int i = 0; i < n; ++i) cin >> arr[i];
 
-        // Read in numbers
-        vector<int> v;
-        for(int i = 0; i < n; i++) {
-            int temp;
-            cin >> temp;
-            v.push_back(temp);
-        }
+        // Get LIS
+        vector<int> lis = LIS(arr);
 
-        // Initialize result array
-        vector<int> r;
-        r.resize(n+1, -1);
-
-        // Initialize tail array
-        vector<int> t;
-        t.resize(n+1, 0);
-
-        // Calculate LIS
-        int l = 1;
-        for(int i = 1; i < n; i++) {
-            if(v[i] < v[t[0]]) {
-                t[0] = i;
-            }
-            else if(v[i] > v[t[l-1]]) {
-                r[i] = t[l-1];
-                t[l] = i;
-                l++;
-            }
-            else {
-                int pos = search(v, t, -1, l-1, v[i]);
-                r[i] = t[pos-1];
-                t[pos] = i;
-            }
-        }
-
-        // Print length
-        cout << l << endl;
-
-        // Calculate LIS
-        vector<int> ans;
-        for(int i = t[l-1]; i >= 0; i = r[i]) {
-            ans.push_back(i);
-        }
-        reverse(ans.begin(), ans.end());
-
-        // Print LIS
-        for(auto i : ans) {
+        // Print answer
+        cout << lis.size() << endl;
+        for(auto i : lis) {
             cout << i << " ";
         }
         cout << endl;
     }
+    return 0;
 }
