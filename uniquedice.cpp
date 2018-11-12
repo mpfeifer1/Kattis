23300d67617473923acae90b12aaf0b2066b7c1d
Michael Pfeifer <mikepf97@gmail.com>
2018-11-12 02:43:27 -0700

    Solved Unique Dice

---
+++ uniquedice.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string up(string s) {
+    int t = s[0];
+    s[0] = s[2];
+    s[2] = s[1];
+    s[1] = s[3];
+    s[3] = t;
+    return s;
+}
+
+string ri(string s) {
+    int t = s[2];
+    s[2] = s[5];
+    s[5] = s[3];
+    s[3] = s[4];
+    s[4] = t;
+    return s;
+}
+
+string fr(string s) {
+    int t = s[0];
+    s[0] = s[5];
+    s[5] = s[1];
+    s[1] = s[4];
+    s[4] = t;
+    return s;
+}
+
+string re(string s) {
+    set<string> v;
+
+    queue<string> q;
+    q.push(s);
+    while(!q.empty()) {
+        string curr = q.front();
+        q.pop();
+
+        if(v.count(curr)) continue;
+        v.insert(curr);
+
+        q.push(ri(curr));
+        q.push(up(curr));
+        q.push(fr(curr));
+    }
+
+    return *(v.begin());
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    map<string,int> m;
+    for(int i = 0; i < n; i++) {
+        string s = "      ";
+        for(auto& j : s) {
+            cin >> j;
+        }
+
+        s = re(s);
+        m[s]++;
+    }
+
+    int best = 1;
+    for(auto i : m) {
+        best = max(best, i.second);
+    }
+
+    cout << best << endl;
+}
