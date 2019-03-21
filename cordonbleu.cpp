980f0050e53eb5a7a21ee55e6e53a2189182e139
Michael Pfeifer <mikepf97@gmail.com>
2019-03-20 18:03:30 -0600

    Solved Cordon Bleu using e-maxx.ru hungarian

--- cordonbleu.cpp
+++ cordonbleu.cpp
@@ -2,75 +2,60 @@
 
 using namespace std;
 typedef long long ll;
-const ll inf = (ll)1 << 60;
+ll inf = (ll)1 << 60;
 
-struct edge {
-	ll b, u, c, f;
-	size_t back;
-};
+// this is one-indexed
+// jobs X workers cost matrix
+// cost[i][j] is cost of job i done by worker j
+// #jobs must be <= #workers
+// Default finds min cost; to find max cost set all costs[i][j] to -costs[i][j]
+ll HungarianMatch(const vector<vector<ll>>& a) {
+    ll n = a.size()-1;
+    ll m = a[0].size()-1;
+    vector<ll> u(n+1), v(m+1), p(m+1), way(m+1);
+    for(ll i = 1; i <= n; ++i) {
+        p[0] = i;
+        ll j0 = 0;
+        vector<ll> minv(m+1, inf);
+        vector<char> used(m+1, false);
+        do {
+            used[j0] = true;
+            ll i0 = p[j0], delta = inf, j1;
+            for(ll j = 1; j <= m; ++j)
+                if(!used[j]) {
+                    ll cur = a[i0][j] - u[i0] - v[j];
+                    if(cur < minv[j])
+                        minv[j] = cur, way[j] = j0;
+                    if(minv[j] < delta)
+                        delta = minv[j], j1 = j;
+                }
+            for(ll j = 0; j <= m; ++j)
+                if(used[j])
+                    u[p[j]] += delta, v[j] -= delta;
+                else
+                    minv[j] -= delta;
+            j0 = j1;
+        } while(p[j0] != 0);
+        do {
+            ll j1 = way[j0];
+            p[j0] = p[j1];
+            j0 = j1;
+        } while(j0);
+    }
 
-void addedge(vector<vector<edge>>& g, ll a, ll b, ll u, ll c) {
-	edge r1 = {b,u,c,0,g[b].size()};
-	edge r2 = {a,0,-c,0,g[a].size()};
-	g[a].push_back(r1);
-	g[b].push_back(r2);
-}
+    /*
+    // For each N, it contains the M it selected
+    vector<ll> ans(n+1);
+    for(ll j = 1; j <= m ; ++j)
+        ans[p[j]] = j;
+    */
 
-ll n, m, k = inf;
-
-pair<ll,ll> mincostflow(vector<vector<edge>>& g, ll s, ll t) {
-	ll flow = 0,  cost = 0;
-	while(flow < k) {
-		vector<ll> id(n, 0);
-		vector<ll> d(n, inf);
-		vector<ll> q(n);
-		vector<ll> p(n);
-		vector<size_t> p_edge(n);
-		ll qh=0, qt=0;
-		q[qt++] = s;
-		d[s] = 0;
-		while(qh != qt) {
-			ll v = q[qh++];
-			id[v] = 2;
-			if(qh == n)  qh = 0;
-			for(size_t i=0; i<g[v].size(); ++i) {
-				edge& r = g[v][i];
-				if(r.f < r.u && d[v] + r.c < d[r.b]) {
-					d[r.b] = d[v] + r.c;
-					if(id[r.b] == 0) {
-						q[qt++] = r.b;
-						if(qt == n) qt = 0;
-					}
-					else if(id[r.b] == 2) {
-						if(--qh == -1) qh = n-1;
-						q[qh] = r.b;
-					}
-					id[r.b] = 1;
-					p[r.b] = v;
-					p_edge[r.b] = i;
-				}
-			}
-		}
-		if(d[t] == inf) break;
-		ll addflow = k - flow;
-		for(ll v=t; v!=s; v=p[v]) {
-			ll pv = p[v]; size_t pr = p_edge[v];
-			addflow = min(addflow, g[pv][pr].u - g[pv][pr].f);
-		}
-		for(ll v=t; v!=s; v=p[v]) {
-			ll pv = p[v]; size_t pr = p_edge[v], r = g[pv][pr].back;
-			g[pv][pr].f += addflow;
-			g[v][r].f -= addflow;
-			cost += g[pv][pr].c * addflow;
-		}
-		flow += addflow;
-	}
-    return {flow,cost};
+    return -v[0];
 }
 
 ll dist(pair<ll,ll>& p1, pair<ll,ll>& p2) {
     return abs(p1.first-p2.first) +
-           abs(p1.second-p2.second);
+        abs(p1.second-p2.second);
 }
 
 int main(){
@@ -89,47 +74,26 @@ int main(){
     pair<ll,ll> restaraunt;
     cin >> restaraunt.first >> restaraunt.second;
 
-    ll sz = n + n+m-1 +2;
-    ::n = sz;
-    ::m = sz;
-
-	vector<vector<edge>> g(::n);
-
-    ll s = sz - 1;
-    ll t = sz - 2;
-
-    // Connect source to each bottle
-    for(ll i = 0; i < n; i++) {
-        addedge(g,s,i,1,0);
-    }
-
-    // Connect each courier to the sink
-    for(ll j = 0; j < n+m-1; j++) {
-        ll a = j+n;
-        addedge(g,a,t,1,0);
-    }
+    vector<vector<ll>> costs;
+    costs.resize(n+1,vector<ll>(n+m));
 
     // For each bottle
     for(ll i = 0; i < n; i++) {
         // For each courier
         for(ll j = 0; j < n+m-1; j++) {
-            ll a = j + n;
+            ll costhere = 0;
             // Actual courier
             if(j < m) {
-                ll costhere = 0;
                 costhere += dist(bottles[i],couriers[j]);
                 costhere += dist(bottles[i],restaraunt);
-                addedge(g,i,a,1,costhere);
             }
-            // Restaraunt and back
+            // Restaurant and back
             else {
-                ll costhere = 0;
                 costhere += 2 * dist(bottles[i],restaraunt);
-                addedge(g,i,a,1,costhere);
             }
+            costs[i+1][j+1] = costhere;
         }
     }
 
-    pair<ll,ll> p1 = mincostflow(g, s, t);
-    cout << p1.second << endl;
+    cout << HungarianMatch(costs) << endl;
 }
